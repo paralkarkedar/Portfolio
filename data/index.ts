d0b0f829ed79d0909bf2e440f6d13fda07421ce1
kedar paralkar <i.485.orizzonte@gmail.com>
2024-05-24 10:58:28 +0530

    My Portfolio

---
+++ data/index.ts
@@ -0,0 +1,197 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Internship", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "Proficient in programming languages such as Python, Java and JavaScript. ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building New React Js Projects",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to hire Me ?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Android Application",
+    des: "I have created an application for farmers to connect with customers.",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/android_logo.png", "/ts.svg"],
+    link: "https://github.com/paralkarkedar/farmeasy",
+  },
+  {
+    id: 2,
+    title: "MITian'S",
+    des: "Mitian's app is used by college students to find and research colleges.",
+    img: "/p2.svg",
+    iconLists: ["/re.svg", "/android_logo.png", "/ts.svg"],
+    link: "https://github.com/paralkarkedar/farmeasy",
+  },
+
+
+];
+
+export const testimonials = [
+  {
+    quote:
+      "During my four-month internship at Ravik Foundation, I gained comprehensive experience in both backend and frontend development using Laravel technology. I implemented Google reCAPTCHA to enhance security and prevent spam, and I developed and integrated an authentication system to streamline user access and improve security. I collaborated with the development team to build and maintain robust web applications and assisted in troubleshooting and resolving issues, contributing to the overall success of various projects.",
+    name: "Kedar Paralkar",
+    title: "Intern at Ravik Foundation",
+  },
+  {
+    quote:
+      "At Variant Labs, I worked as an application developer where I gained valuable experience in production-level application management. I learned advanced concepts in Android application development and became proficient in using Git and GitHub for version control. Additionally, I expanded my knowledge of SQL databases, which enhanced my ability to manage and manipulate data effectively. This role provided me with a strong foundation in both development and application management, contributing significantly to my professional growth.",
+    name: "Kedar Paralkar",
+    title: "Application Developer at Variant Labs",
+  },
+  {
+    quote:
+      "I joined the Freshers Incubation Program at Profound Edu Tech, a highly selective program offered free for few students. During this program, I had the opportunity to learn a range of technologies and programming languages, including Java, advanced Java, ODBC, JDBC, and SQL databases. This experience provided me with a solid foundation in both basic and advanced concepts, enhancing my skills in software development and database management.",
+    title: "Freshers Incubation Program at Profound Edu Tech",
+  },
+  {
+    quote:
+      "Currently, I am working at Orizzonte Consultancy Services where I am learning and utilizing frameworks like Django to create websites and projects for the organization. I have contributed to various projects, enhancing my technical skills and gaining practical experience in web development. Additionally, I have improved my communication skills by collaborating with team members and stakeholders, ensuring effective project delivery and client satisfaction.",
+    name: "Kedar Paralkar",
+    title: "Software Engineer at Orizzonte Consultancy Services",
+  },
+ 
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "sndroid.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Mobile App Dev - BigInt Tech",
+    desc: "Designed and developed mobile app for  Android platforms using React Native & Android .",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance App Dev Project",
+    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
